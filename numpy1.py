a0395474fe675236e7cc5e5f2ca4606683a23430
Sneha41sb <snehasbiju2004@gmail.com>
2026-09-10 21:30:51 +0530

    numpy

---
+++ numpy1.py
@@ -0,0 +1,9 @@
+import numpy as np
+a=np.array([10,20,30,40,50])
+print(a)
+print(type(a))
+print(a[0])
+print(a[-1]) #last
+print(a[1:3])#1st nd 2nd
+print(a * 2)          # [20 40 60 80 100]
+print(a + 5) 
